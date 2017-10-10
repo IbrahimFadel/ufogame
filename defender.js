ff699456da295a11b099668d47224056b1941262
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2017-10-10 14:34:20 -0700

    Added explosion(on fuel running out), fuel bar logic, added baddies collision logic

--- defender.js
+++ defender.js
@@ -3,10 +3,13 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload:
 function preload () {
 
     game.load.image('player', 'assets/games/defender/ship.png');
+    game.load.image('newPlayer', 'assets/sprites/ufo.png');
     game.load.image('star', 'assets/demoscene/star2.png');
     game.load.image('baddie', 'assets/sprites/space-baddie.png');
+    game.load.image('fuelTank', 'assets/sprites/orb-red.png');
     game.load.atlas('lazer', 'assets/games/defender/laser.png', 'assets/games/defender/laser.json');
     game.load.spritesheet('kaboom', 'assets/games/invaders/explode.png', 128, 128);
+    game.load.bitmapFont('stack', 'assets/demoscene/shortStack.png', 'assets/fonts/bitmapFonts/shortStack.xml');
 }
 
 var stars;
@@ -22,10 +25,12 @@ var score = 0;
 var fuelAmount = 200;
 var explosion;
 var hasBeenCalled = false;
+var ammunition = 200;
+var textFuel;
+var textAmmunition;
 
-function create () {
+    function create () {
 
-    game.world.setBounds(0, 0, 800, 600);
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.world.setBounds(0, 0, 800*4, 600);
@@ -53,10 +58,21 @@ function create () {
     lazers.enableBody = true;
     lazers.physicsBodyType = Phaser.Physics.ARCADE;
 
-    player = game.add.sprite(100, 300, 'player');
+    fuelTank = game.add.sprite(200, 200, 'fuelTank');
+    game.physics.arcade.enable(fuelTank);
+    fuelTank.anchor.x = 0.5;
+    fuelTank.collideWorldBounds = true;
+
+    for (let i = 0; i < 5; i++)
+    {
+        baddies.create(game.world.randomX, game.world.randomY, 'fuelTank');
+    }
+
+    player = game.add.sprite(100, 300, 'newPlayer');
     game.physics.arcade.enable(player);
     player.anchor.x = 0.5;
     player.body.collideWorldBounds = true;
+    player.body.gravity.y = 100;
 
     player.animations.add('kaboom');
     explosion = game.add.sprite(-100,-100, 'kaboom');
@@ -68,68 +84,103 @@ function create () {
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     prevCamX = game.camera.x;
-}
+
+        var textFuel = game.add.bitmapText(100, 100, 'stack', 'Fuel', 64);
+    }
 
 function update () {
     game.physics.arcade.overlap(lazers, baddies, function(lazer,baddie){
         baddie.kill();
         score++
-        console.log("A baddie was hit!")
+
     }, null, this);
 
     game.physics.arcade.overlap(player, baddies, function(player, baddie){
-        console.log("Whoops! watch where ur goin")
+
     }, null, this);
 
+    game.physics.arcade.overlap(player, fuelTank, function(player, fuel) {
+        if (fuelAmount > 149) {
+            fuelTank.kill();
+            fuelAmount = fuelAmount + (200-fuelAmount)
+        } else if (fuelAmount <= 149) {
+            fuelTank.kill();
+            fuelAmount = fuelAmount + 50;
+        }
+    })
 
     var graphics = game.add.graphics(100, 100);
     graphics.fixedToCamera = true;
-    graphics.beginFill(0xFFFFFF);
+    graphics.beginFill(255, 255, 255);
 
 
     graphics.clear();
     graphics.endFill();
     graphics.lineStyle(2, 0x0000FF, 1);
-    graphics.drawRect( 50, 10, 200, 100);
+    graphics.drawRect( -70, -70, 200, 20);
     graphics.beginFill(0xFFFFFF);
-    graphics.drawRect( 50, 10, fuelAmount, 100);
+    graphics.drawRect( -70, -70, fuelAmount, 20);
 
 
     if(fuelAmount <= 0 && hasBeenCalled == false) {
         explosion.reset(player.x - 70, player.y - 50);
         explosion.animations.play('explode', 30, false, true);
+        player.kill();
         hasBeenCalled = true
     }
 
     if(fuelAmount > 0) {
         if(cursors.left.isDown || cursors.right.isDown || cursors.up.isDown || cursors.down.isDown ) {
-            fuelAmount = fuelAmount - 0.5
+            fuelAmount = fuelAmount - 0.1
         }
         if(cursors.left.isDown)
         {
-            player.x -= 8;
+            player.body.velocity.x = player.body.velocity.x - 10;
             player.scale.x = -1;
         }
         else if(cursors.right.isDown)
         {
-            player.x += 8;
+            player.body.velocity.x = player.body.velocity.x + 10;
             player.scale.x = 1;
         }
 
         if(cursors.up.isDown)
         {
-            player.y -= 8;
+            //player.y -= 8;
+            player.body.velocity.y = player.body.velocity.y - 10;
         }
         else if(cursors.down.isDown)
         {
-            player.y += 8;
+            player.body.velocity.y = player.body.velocity.y + 10;
         }
+        if(ammunition <= 0) {
+            ammunition = 0;
+        } else if(ammunition > 0) {
+            var graphics = game.add.graphics(100, 100);
+            graphics.fixedToCamera = true;
+            graphics.beginFill(0xFFFFFF);
 
-        if(fireButton.isDown)
-        {
-            fireBullet();
+
+            graphics.clear();
+            graphics.endFill();
+            graphics.lineStyle(2, 0x0000FF, 1);
+            graphics.drawRect( 300, -70, 200, 20);
+            graphics.beginFill(0xFFFFFF);
+            graphics.drawRect( 300, -70, ammunition, 20);
+
+            if(fireButton.isDown)
+            {
+                fireBullet();
+                ammunition = ammunition - 1;
+                if (player.scale.x === 1) {
+                    player.body.velocity.x = player.body.velocity.x - 5;
+                } else if (player.scale.x === -1) {
+                    player.body.velocity.x = player.body.velocity.x + 5;
+                }
+            }
         }
     }
+
 
 
 
